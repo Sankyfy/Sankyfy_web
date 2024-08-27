3a569f9cc054aff81ab0f5b97bc138186f7a4cce
Akshaypareek01 <akshay96102@gmail.com>
2024-08-28 02:51:15 +0530

    : )

--- src/Configs/BaseUrl.jsx
+++
@@ -1,3 +0,0 @@
-// export const Base_Url = "http://localhost:5000/";
-
-export const Base_Url = "https://sankyfy-backend.onrender.com/";
--- src/Context/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoute = ({children}) => {
-    let isAuth = localStorage.getItem("isLoggedIn") 
-    if(!isAuth) {
-        return <Navigate to={'/'}/>
-    }
-    return children;
-}
